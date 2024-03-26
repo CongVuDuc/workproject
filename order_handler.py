424e3fc211863eecb083015cb388450b8e84689d
yaokiet <ykleong.2022@scis.smu.edu.sg>
2024-03-26 18:03:02 +0800

    update
    order handler and relevant AMQP files

---
+++ order_handler.py
@@ -0,0 +1,146 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+import os, sys
+
+import requests
+from invokes import invoke_http
+
+import pika
+import json
+import amqp_connection
+
+app = Flask(__name__)
+CORS(app)
+
+exchangename = "order_topic" # exchange name
+exchangetype="topic" # use a 'topic' exchange to enable interaction
+
+#create a connection and a channel to the broker to publish messages to activity_log, error queues
+connection = amqp_connection.create_connection() 
+channel = connection.channel()
+
+#if the exchange is not yet created, exit the program
+if not amqp_connection.check_exchange(channel, exchangename, exchangetype):
+    print("\nCreate the 'Exchange' before running this microservice. \nExiting the program.")
+    sys.exit(0)  # Exit with a success status
+
+
+# API endpoint for the order database
+order_URL = "https://personal-4acjyryg.outsystemscloud.com/Order/rest/v1/order/"
+
+# Route for updating order status
+@app.route("/order_handler", methods=['POST'])
+def order_handler():
+    # Simple check of input format and data of the request are JSON
+    if request.is_json:
+        try:
+            order = request.get_json()
+            print("\nReceived an order in JSON:", order)
+
+            # do the actual work
+            # 1. Send order info {cart items}
+            result = processOrderHandler(order)
+            print(result)
+            return jsonify(result), result["Status_code"]
+
+        except Exception as e:
+            # Unexpected error in code
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            ex_str = str(e) + " at " + str(exc_type) + ": " + fname + ": line " + str(exc_tb.tb_lineno)
+            print(ex_str)
+
+            return jsonify({
+                "code": 500,
+                "message": "place_order.py internal error: " + ex_str
+            }), 500
+
+
+    # if reached here, not a JSON request.
+    return jsonify({
+        "code": 400,
+        "message": "Invalid JSON input: " + str(request.get_data())
+    }), 400
+
+def processOrderHandler(order):
+    # 2. Send the order info {cart items}
+    # Invoke the order microservice
+    print('\n-----Invoking order microservice-----')
+    userid = order["order_id"]
+    order_status = order["order_status"]
+    print(userid, order_status)
+    order_URL = f"https://personal-4acjyryg.outsystemscloud.com/Order/rest/v1/order/status/{userid}/{order_status}/"
+    print(order_URL)
+    order_result = invoke_http(order_URL,method="PUT")
+    print('order_result:', order_result)
+
+    # Check the order result; if a failure, send it to the error microservice.
+    code = order_result["Status_Code"]
+    print("checking: ", code)
+    message = json.dumps(order_result)
+
+    if code not in range(200, 300):
+        # Inform the error microservice
+        #print('\n\n-----Invoking error microservice as order fails-----')
+        print('\n\n-----Publishing the (order error) message with routing_key=order.error-----')
+
+        # invoke_http(error_URL, method="POST", json=order_result)
+        channel.basic_publish(exchange=exchangename, routing_key="order.error", 
+            body=message, properties=pika.BasicProperties(delivery_mode = 2)) 
+        # make message persistent within the matching queues until it is received by some receiver 
+        # (the matching queues have to exist and be durable and bound to the exchange)
+
+        # - reply from the invocation is not used;
+        # continue even if this invocation fails        
+        print(f"\nOrder status ({code}) published to the RabbitMQ Exchange:", order_result)
+
+        # 7. Return error
+        return {
+            "Status_code": 500,
+            "data": {"order_result": order_result},
+            "message": "Order creation failure sent for error handling."
+        }
+
+    # Notice that we are publishing to "Activity Log" only when there is no error in order creation.
+    # In http version, we first invoked "Activity Log" and then checked for error.
+    # Since the "Activity Log" binds to the queue using '#' => any routing_key would be matched 
+    # and a message sent to “Error” queue can be received by “Activity Log” too.
+
+    else:
+        # 4. Record new order
+        # record the activity log anyway
+        #print('\n\n-----Invoking activity_log microservice-----')
+        print('\n\n-----Publishing the (order info) message with routing_key=order.info-----')        
+
+        # invoke_http(activity_log_URL, method="POST", json=order_result)            
+        channel.basic_publish(exchange=exchangename, routing_key="order.info", 
+            body=message)
+
+        print("\nOrder published to RabbitMQ Exchange.\n")
+    # - reply from the invocation is not used;
+    # continue even if this invocation fails
+
+
+    # 7. Return created order
+    return {
+        "Status_code": 201,
+        "data": {
+            "order_result": order_result,
+        }
+    }
+    
+
+# Execute this program if it is run as a main script (not by 'import')
+if __name__ == "__main__":
+    print("This is flask " + os.path.basename(__file__) +
+          " for placing an order...")
+    app.run(host="0.0.0.0", port=5100, debug=True)
+    # Notes for the parameters:
+    # - debug=True will reload the program automatically if a change is detected;
+    #   -- it in fact starts two instances of the same flask program,
+    #       and uses one of the instances to monitor the program changes;
+    # - host="0.0.0.0" allows the flask program to accept requests sent from any IP/host (in addition to localhost),
+    #   -- i.e., it gives permissions to hosts with any IP to access the flask program,
+    #   -- as long as the hosts can already reach the machine running the flask program along the network;
+    #   -- it doesn't mean to use http://0.0.0.0 to access the flask program.
